ebff747d9ce3ba965f7f7e7705a8bb9db754e5ca
Peyton Casper <peytoncas@gmail.com>
2016-02-29 08:14:06 -0600

    Added functioning ZMQ files

---
+++ zmq demo/CMakeFiles/zmq_cv_request.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zmq_cv_request.dir/zmq_cv_request.cpp.o"
+  "zmq_cv_request.pdb"
+  "zmq_cv_request"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zmq_cv_request.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zmq demo/CMakeFiles/zmq_cv_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zmq_cv_server.dir/zmq_cv_server.cpp.o"
+  "zmq_cv_server.pdb"
+  "zmq_cv_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zmq_cv_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
